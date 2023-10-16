fac6a1e707f0933d9a84ff42abe0b91e1d6c73a1
zielinski-m <122398900+zielinski-m@users.noreply.github.com>
2023-10-16 18:04:07 +0000

    Updated app - Replit commit

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
 
-    const apiRoot =  'http://localhost:8080/v1/tasks';
+    const apiRoot =  'https://task-web-application.michalzielins12.repl.co/v1/tasks';
     const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
     const tasksContainer = $('[data-tasks-container]');
 
