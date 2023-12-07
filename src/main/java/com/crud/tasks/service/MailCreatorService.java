5e08597016ebfd03b09f17cb6bbe3f7b4c05ebfd
Michal Zielinski <zielinski93.m@gmail.com>
2023-12-07 21:09:54 +0100

    32.3 Assignment

--- src/main/java/com/crud/tasks/service/MailCreatorService.java
+++ src/main/java/com/crud/tasks/service/MailCreatorService.java
@@ -2,6 +2,7 @@ package com.crud.tasks.service;
 
 import com.crud.tasks.config.AdminConfig;
 import com.crud.tasks.config.CompanyInfoProperties;
+import com.crud.tasks.repository.TaskRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,10 @@ public class MailCreatorService {
     @Autowired
     @Qualifier("templateEngine")
     private TemplateEngine templateEngine;
+
+    @Autowired
+    private TaskRepository taskRepository;
+
 
     public String buildTrelloCardEmail(String message) {
 
@@ -49,14 +54,25 @@ public class MailCreatorService {
         return templateEngine.process("mail/created-trello-card-mail", context);
     }
 
+    public String taskCount() {
+        long size = taskRepository.count();
+        String taskCountCondition = (size == 1) ? "task" : "tasks";
+        return "Currently in database you've got: " + size + taskCountCondition;
+    }
+
     public String buildDailyTasksSummaryEmail(String message) {
+
         Context context = new Context();
+        context.setVariable("message", message);
+        context.setVariable("mail_title", "Daily task summary");
         context.setVariable("admin_name", adminConfig.getAdminName());
-        context.setVariable("task_count", message);
+        context.setVariable("task_count", taskCount());
         context.setVariable("company_info",
                 "COMPANY: " + companyInfoProperties.getName() + "\n" +
                         "\u2709: " + companyInfoProperties.getEmail() + "\n" +
                         "\u260e: " + companyInfoProperties.getPhone());
-        return templateEngine.process("mail/daily-task-summary-mail", context);
+        context.setVariable("footer", "Thanks for visiting us!");
+        context.setVariable("isCustomer", true);
+        return templateEngine.process("mail/daily-task-summary", context);
     }
 }
--- src/main/java/com/crud/tasks/service/SimpleMailService.java
+++ src/main/java/com/crud/tasks/service/SimpleMailService.java
@@ -30,9 +30,18 @@ public class SimpleMailService {
             messageHelper.setText(mailCreatorService.buildTrelloCardEmail(mail.getMessage()), true);
         };
     }
+
+    private MimeMessagePreparator createSummaryMessage(final Mail mail) {
+        return mimeMessage -> {
+            MimeMessageHelper helper = new MimeMessageHelper(mimeMessage, true, "UTF-8");
+            helper.setTo(mail.getMailTo());
+            helper.setSubject(mail.getSubject());
+            helper.setText(mailCreatorService.buildDailyTasksSummaryEmail(mail.getMessage()), true);
+        };
+    }
     public void send(final Mail mail) {
         try {
-            javaMailSender.send(createMimeMessage(mail));
+            javaMailSender.send(createSummaryMessage(mail));
             log.info("Email has been sent");
         } catch (Exception e) {
             log.error("Failed to process email sending: + e.getMessage(), e");
